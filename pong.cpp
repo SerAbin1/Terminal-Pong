f9bce61c25e8a15bd180709136f5daab50b07cee
SerAbin1 <abinyt1234@gmail.com>
2025-01-07 21:27:35 +0530

    Add ball with collision detection. Issues: 1. Collision not being detected with top_plank. 2. Ball speed needs to be reduced.

--- pong.cpp
+++ pong.cpp
@@ -1,3 +1,14 @@
+/*
+1. A ball_direction struct to keep track of ball_direction
+2. Initialize ball_direction to right
+Lets keep things simple for now. If the ball hits the left plank it goes left diagonally.
+if the ball its center, it goes straight.
+if right, right diaginally
+3. if ball.x equal to plank[0].x then ball_direction is left diagonal
+*/
+
+//Note to self: remove for loop in checking plank collison with wall
+
 #include <ncurses.h>
 #include <cstdlib>
 #include <vector>
@@ -25,10 +36,9 @@ int main () {
 void game(){
     //Initialize game variables
     Coordinate ball;
+    Coordinate ball_direction;
     std::vector<Coordinate> bottom_plank(3);
     std::vector<Coordinate> top_plank(3);
-    int bottom_plank_direction = 1; //Initial direction is right
-    int top_plank_direction = 1; //Initial direction is right
     int max_rows, max_columns;
     getmaxyx(stdscr, max_rows, max_columns);
     int left = 0;
@@ -37,6 +47,18 @@ void game(){
     int bottom = max_rows - 1;
     int center_rows = max_rows / 2;
     int center_columns = max_columns / 2;
+
+    //set initial directions
+    int top_plank_direction = 1; //Initial direction is right
+    int bottom_plank_direction = 1; //Initial direction is right
+    ball_direction.vertical = 1; //down
+    ball_direction.horizontal = 0; //no movement
+
+    //set initial ball position at the center
+    ball.vertical = center_rows;
+    ball.horizontal = center_columns;
+
+    //Initialize the planks
     for (int i = 0; i < 3; i++) {
         //bottom_plank starts at bottom center
         bottom_plank[i].horizontal = center_columns + i;
@@ -57,28 +79,75 @@ void game(){
         //Get top_plank_direction
         topPlankDirection(top_plank_direction, ball, center_columns);
 
+        //Move ball according to the direction
+        ball.vertical += ball_direction.vertical;
+        ball.horizontal += ball_direction.horizontal;
+
         //Move plank according to the direction
         for (int i = 0; i < 3; i++) {
             bottom_plank[i].horizontal += bottom_plank_direction;
             top_plank[i].horizontal += top_plank_direction;
         }
         
-        //Check for collison with the Wall
+        //Check for plank collision with wall
         for (int i = 0; i < 3; i++) {
-            if (bottom_plank[i].horizontal == 0) {
+            if (bottom_plank[i].horizontal == left) {
                 bottom_plank_direction = 1; //change direction to right
             } else if (bottom_plank[i].horizontal == max_columns - 3) {
                 bottom_plank_direction = -1; //change direction to left
             }
             //check for the top plank
-            if (top_plank[i].horizontal == 0) {
+            if (top_plank[i].horizontal == left) {
                 top_plank_direction = 1; //change direction to right
             } else if (top_plank[i].horizontal == max_columns - 3) {
                 top_plank_direction = -1; //change direction to left
             }
         }
-        
+
+        //Check for ball collision with wall
+        if (ball.horizontal == left + 1) {
+            ball_direction.horizontal = 1; //Direction to right
+        }
+        else if (ball.horizontal == max_columns -1) {
+            ball_direction.horizontal = -1; //Direction to left
+        }
+        //Check for bottom_plank collison with ball
+        if (ball.horizontal == bottom_plank[0].horizontal && ball.vertical == bottom_plank[0].vertical) {
+            //left diagonal if collided with left side of the bottom plank
+            ball_direction.vertical = -1; //up
+            ball_direction.horizontal = -2; //left
+        }
+        else if (ball.horizontal == bottom_plank[1].horizontal && ball.vertical == bottom_plank[1].vertical) {
+            //straight up if collided with centre of the plank
+            ball_direction.vertical = -1; //up
+            ball_direction.horizontal = 0; //nill
+        }
+        else if (ball.horizontal == bottom_plank[2].horizontal && ball.vertical == bottom_plank[2].vertical) {
+            //right diagonal if collided with centre of the plank
+            ball_direction.vertical = -1; //up
+            ball_direction.horizontal = 2; //right
+        }
+        //Check for top_plank collison with plank
+        if (ball.horizontal == top_plank[0].horizontal && ball.vertical == top_plank[0].vertical) {
+            //left diagonal if collided with left side of the bottom plank
+            ball_direction.vertical = 1; //down
+            ball_direction.horizontal = -2; //left
+        }
+        else if (ball.horizontal == top_plank[1].horizontal && ball.vertical == top_plank[1].vertical) {
+            //straight down if collided with centre of the plank
+            ball_direction.vertical = 1; //up
+            ball_direction.horizontal = 0; //nill
+        }
+        else if (ball.horizontal == top_plank[2].horizontal && ball.vertical == top_plank[2].vertical) {
+            //right diagonal if collided with centre of the plank
+            ball_direction.vertical = 1; //up
+            ball_direction.horizontal = 2; //right
+        }
+
         clear();
+        //Render the ball
+        mvprintw(ball.vertical, ball.horizontal, "O");
+
         //Render the planks
         for (int i = 0; i < 3; i++) {
             mvprintw(bottom_plank[i].vertical, bottom_plank[i].horizontal, "-");
